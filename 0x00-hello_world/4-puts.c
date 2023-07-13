dcc4f1b4be69eed8bf228c0c85159ba439605632
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-07-13 22:24:44 +0300

    a C program that prints exactly "Programming is like building a multilingual puzzle", followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Retrun: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
