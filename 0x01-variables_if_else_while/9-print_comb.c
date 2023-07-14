65175612559004ebc8414861605fdbff00346793
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-07-14 23:26:23 +0300

    Program that prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase and uppercase in the same line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+			putchar(',');
+		if (i != 9)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
