d45c12c8213ccf0aacae4a90d87e7473ba835fce
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-07-19 19:25:18 +0300

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the sum of all the multiples of 3 or 5 below 1024
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int i, x = 0, y = 1, z, sum = 0;
+
+	z = x + y;
+	for (i = 0; i <= 4000000; i++)
+	{
+		if (z % 2 == 0)
+			sum += z;
+		x = y;
+		y = z;
+		z = x + y;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
