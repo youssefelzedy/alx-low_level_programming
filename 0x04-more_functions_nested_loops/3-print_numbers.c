665028bcad3bb5acd0f0401d12484a363851eed3
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-07-20 13:21:25 +0300

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 /**
  * print_numbers -  function that prints the numbers, from 0 to 9, followed by a new line
  *
- * Return : no return (Success)
+ * Return: no return (Success)
  */
 
 void print_numbers(void)
