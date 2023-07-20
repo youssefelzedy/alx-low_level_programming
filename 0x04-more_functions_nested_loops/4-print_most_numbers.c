bd3dd29e9cc7cb996e480d49986b2f6754229e90
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-07-20 13:26:54 +0300

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - function that prints the numbers, from 0 to 9, followed by a new line
- * 
+ * print_most_numbers - function that prints the numbers, from 0 to 9
  * Return: no return (Success)
  */
 
