5b3a568deba0186e18d05f004006ea40e09cdefe
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-07-20 16:50:24 +0300

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,36 +15,33 @@ void print_number(int n)
 	{
 		_putchar('0');
 	}
-	else
+	if (n == -2147483648)
 	{
-		if (n == -2147483648)
-		{
-			n = 147483648;
-			_putchar('-');
-			_putchar('2');
-		}
-		if (n >= 1000000000)
-		{
-			_putchar('0' + n / 1000000000);
-			n -= (n / 1000000000) * 1000000000;
-		}
-		if (n < 0)
-		{
-			_putchar('-');
-			n = -n;
-		}
-		gg = n;
-		while (gg != 0)
-		{
-			gg /= 10;
-			mod *= 10;
-		}
-		while (n > 0)
-		{
-			mod /= 10;
-			test = n / mod;
-			n %= mod;
-			_putchar(test + '0');
-		}
+		n = 147483648;
+		_putchar('-');
+		_putchar('2');
+	}
+	if (n >= 1000000000)
+	{
+		_putchar('0' + n / 1000000000);
+		n -= (n / 1000000000) * 1000000000;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	gg = n;
+	while (gg != 0)
+	{
+		gg /= 10;
+		mod *= 10;
+	}
+	while (n > 0)
+	{
+		mod /=10;
+		test = n / mod;
+		n %= mod;
+		_putchar(test + '0');
 	}
 }
