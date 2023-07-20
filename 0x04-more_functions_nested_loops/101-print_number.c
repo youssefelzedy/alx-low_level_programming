a6ec7eac87c5d013e8350a5bcfd7332d1796713a
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-07-20 16:31:21 +0300

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_number -  prints the number
+ *@n: is an long long int 
+ *Return: retrun the bigest facor num
+ */
+
+void print_number(int n)
+{
+	int test, mod = 1, gg;
+
+	if(n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+		gg = n;
+		while (gg != 0)
+		{
+			gg /= 10;
+			mod *= 10;
+		}
+		/*printf("mod %d num %d \n", mod, n);*/
+		while (n > 0)
+		{
+			mod /= 10;
+			test = n / mod;
+			n %= mod;
+			/*printf("moz %d num %d \n", mod, n);*/
+			_putchar(test + '0');
+		}
+	}
+}
