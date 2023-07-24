f8e22faa97a16ac3536243dfe968b0bc114c44bb
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-07-24 23:20:02 +0300

    Write a function that convert a string to an integer.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *_atoi -  print n number in array
+ *@s: is a string
+ *Return: the convert number
+ */
+
+int _atoi(char *s)
+{
+	int i;
+
+	i = atoi(s);
+	return (i);
+}
