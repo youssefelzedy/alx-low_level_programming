04cdc421d89d1a0c7c16cd16db3a459ce7a62b11
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-05 01:49:46 +0300

    0x0A-argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,7 +9,7 @@
  * Return: Always 0 (Success)
  */
 
-int main(int rgc, char __attribute__((unused)) *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc);
 	return (0);
