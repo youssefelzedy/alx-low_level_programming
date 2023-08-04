3724346420641afd95c90e1e85b64cc64abd87c5
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-05 01:48:24 +0300

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the of program
+ * @argc: argument count
+ * @argv: arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int __attribute__((unused)) rgc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the of program
+ * @argc: argument count
+ * @argv: arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int rgc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
