2db544a76d52083f7396da1cdfa68dd216038aae
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-05 02:30:27 +0300

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main - adds positive numbers.
+ *@argc: argument count
+ *@argv: arguments
+ *Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, length, sum;
+	char *ptr;
+
+	if (argc < 2)
+		printf("0\n");
+	else
+	{
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
+			{
+				if (!isdigit(*(ptr + j)))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
