6c07fa4c3b12ad66660f66bb0107859f4b707dc7
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-11 01:17:47 +0300

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *_calloc - creat str with in s1 and s2
+ *@nmemb: int
+ *@size: int
+ *Return: nothing;
+ **/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	int cnt = size + nmemb, i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < cnt; i++)
+		ptr[i] = '0';
+
+	ptr[i] = '\0';
+	return (ptr);
+
+}
