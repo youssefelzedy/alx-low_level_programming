e39aa9c8f122f1ab86f8dc3a9b5a4a7896df2e84
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-10 13:32:12 +0300

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * malloc_checked - creat array
+ * @grid: width of the array
+ * @height: height of the array
+ * Return: ptr;
+ **/
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(b);
+
+    if (ptr == 0)
+        exit (98);
+    return (ptr);
+
+}
