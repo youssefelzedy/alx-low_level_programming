ccf5a8f1c4e171ebc3bbed8b44dd931cdcf929ee
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-12 02:43:23 +0300

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,114 +1,114 @@
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
+#include "main.h"
 #include <stdio.h>
-#include <ctype.h>
+#include <string.h>
+
 /**
  * _isdigit - checks if character is digit
  * @c: the character to check
  *
  * Return: 1 if digit, 0 otherwise
  */
-
 int _isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
 }
 
 /**
- * fill - fill ans.
- * @a: int 1
- * @b: int 2
- * @l1: len int 1
- * @l2: len int 2
- * Return: nothing
- */
-
-int *fill(int *a, int *b, int l1, int l2)
-{
-	int tmp, i, j;
-	int *ans = calloc(l1 + l2, sizeof(int));
-
-	if (!ans)
-		printf("Error\n"), exit(98);
-	for (i = 0; i < l2; i++)
-	{
-		for (j = 0; j < l1; j++)
-			ans[i + j] += b[i] * a[j];
-	}
-	for (i = 0; i < l1 + l2; i++)
-	{
-		tmp = ans[i] / 10;
-		ans[i] = ans[i] % 10;
-		ans[i + 1] = ans[i + 1] + tmp;
-	}
-	return (ans);
-}
-/**
- * setdig - check if dig or not
- * @s: the first big number string
- * @l: number
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Return: array
+ * Return: integer length of string
  */
-
-int *setdig(char *s, int l)
+int _strlen(char *s)
 {
-	int i, j, num = l;
-	int *a = malloc(sizeof(int) * l);
+	int i = 0;
 
-	if (a == NULL)
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * big_multiply - multiply two big number strings
+ * @s1: the first big number string
+ * @s2: the second big number string
+ *
+ * Return: the product big number string
+ */
+char *big_multiply(char *s1, char *s2)
+{
+	char *r;
+	int l1, l2, a, b, c, x;
+
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	r = malloc(a = x = l1 + l2);
+	if (!r)
 		printf("Error\n"), exit(98);
-	while (num--)
-		a[num] = 0;
-	for (i = l - 1, j = 0; i >= 0; i--, j++)
+	while (a--)
+		r[a] = 0;
+
+	for (l1--; l1 >= 0; l1--)
 	{
-		if (!_isdigit(s[i]))
+		if (!_isdigit(s1[l1]))
 		{
-			free(a);
+			free(r);
 			printf("Error\n"), exit(98);
 		}
-		a[j] = s[i] - '0';
+		a = s1[l1] - '0';
+		c = 0;
+
+		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+		{
+			if (!_isdigit(s2[l2]))
+			{
+				free(r);
+				printf("Error\n"), exit(98);
+			}
+			b = s2[l2] - '0';
+
+			c += r[l1 + l2 + 1] + (a * b);
+			r[l1 + l2 + 1] = c % 10;
+
+			c /= 10;
+		}
+		if (c)
+			r[l1 + l2 + 1] += c;
 	}
-	return (a);
+	return (r);
 }
 
 
 /**
- *main - multiply two big number strings
- *@argc: argument count
- *@argv: arguments
- *Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ * main - multiply two big number strings
+ * @argc: the number of arguments
+ * @argv: the argument vector
+ *
+ * Return: Always 0 on success.
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i, l1, l2;
-	int *a, *b, *ans;
+	char *r;
+	int a, c, x;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	l1 = strlen(argv[1]);
-	l2 = strlen(argv[2]);
-	a = malloc(sizeof(int) * l1);
-	b = malloc(sizeof(int) * l2);
-	ans = malloc(sizeof(int) * (l1 + l2));
-	if (ans == NULL)
-		printf("Error\n"), exit(98);
-	a = setdig(argv[1], l1);
-	b = setdig(argv[2], l2);
-	ans = fill(a, b, l1, l2);
-	for (i = l1 + l2; i >= 0; i--)
-	{
-		if (ans[i] > 0)
-			break;
-	}
-	for (; i >= 0; i--)
-		printf("%d", ans[i]);
-	printf("\n");
-	free(ans);
-	free(a);
-	free(b);
 
+	x = _strlen(argv[1]) + _strlen(argv[2]);
+	r = big_multiply(argv[1], argv[2]);
+	c = 0;
+	a = 0;
+	while (c < x)
+	{
+		if (r[c])
+			a = 1;
+		if (a)
+			_putchar(r[c] + '0');
+		c++;
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(r);
 	return (0);
 }
