d9c26a4e65a28d9b096d9522c57a61e90f346e2b
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-12 02:47:03 +0300

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 /* declarations and definitions go here */
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _putchar(char);
