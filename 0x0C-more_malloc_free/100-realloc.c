e20c7959b7ba523c72b0fae50c36e7a8f01b35a4
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-12 00:49:30 +0300

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _realloc - add size to array
+ * @ptr: pointer
+ * @old_size: int
+ * @new_size: int
+ *
+ * Return: array
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+	unsigned int i;
+
+	if (old_size == new_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p);
+	}
+	if (old_size < new_size)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i < old_size; i++)
+			*((char *)p + i) = *((char *)ptr + i);
+		free(ptr);
+	}
+	else if (old_size > new_size)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i < new_size; i++)
+			*((char *)p + i) = *((char *)ptr + i);
+		free (ptr);
+	}
+	return (p);
+}
