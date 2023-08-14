4ee927ca8aa261ba121131c8994da538468b2aef
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-14 20:24:08 +0300

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: Just a lone dog struct
+ * */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
