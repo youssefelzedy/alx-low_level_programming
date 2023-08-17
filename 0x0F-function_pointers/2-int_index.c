ea2b894bb8c3d13f582fe090aaaf3fac322151e2
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-18 01:07:59 +0300

    0x0F-function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,17 +16,12 @@ int int_index(int *array, int size, int(*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
 	if (array && size && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(arr[i]))
-			{
 				return (i + 1);
-				break;
-			}
 		}
 	}
 
