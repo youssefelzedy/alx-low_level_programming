37a1b3b91e25e2cc0ed2a6b25e56d5cf07deb268
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-24 20:26:00 +0300

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,6 +18,11 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	newNode->str = strdup(str);
+	if (newNode->str  == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
 	newNode->len = strlen(str);
 	newNode->next = *head;
 	*head = newNode;
