5beb2bd7e6986056cf6928394b3915cd02c7a170
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-24 19:46:01 +0300

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *print_list - prints all the elements of a list_t list.
+ *@h: struct.
+ *Return: ..
+ */
+
+size_t print_list(const list_t *h)
+{
+	int n = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
