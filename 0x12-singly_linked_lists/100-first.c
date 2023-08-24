fa936b0373e9e302004c15a9953fac43b87d50fd
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-25 01:49:22 +0300

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
