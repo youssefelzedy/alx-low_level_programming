845011f449a6d2ff748297faf692682119d66af3
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-24 20:21:34 +0300

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,7 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newNode = (list_t) malloc(sizeof(list_t));
+	list_t *newNode = (list_t *) malloc(sizeof(list_t));
 
 	if (head == NULL)
 	{
