7a33b71e48c0fea04eb45268c8e2c980e75613ef
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-08-31 23:15:40 +0300

    0x14-bit_manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -12,14 +12,5 @@ int get_endianness(void)
 	char *c;
 
 	c = (char *) &i;
-	if (*c == 1)
-	{
-		printf("Little Endian\n");
-	}
-	else
-	{
-		printf("Big Endian\n");
-	}
-
-	return (0);
+	return (c);
 }
