a5a273718b006ff27d51fba2fbe86e4629294674
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-09-05 03:36:35 +0300

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC  | 0600);
-	fw = write(fo, text_content, strlen(text_content) - 1);
+	fw = write(fo, text_content, strlen(text_content) + 1);
 	if (fo == -1 || fw == -1)
 		return (-1);
 
