7c6ee9db52fccc9c9c5af2aa00c8cf77d5841199
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-09-05 03:35:27 +0300

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,13 +10,18 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fo, fw;
+	int fo, fw, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 	fo = open(filename, O_WRONLY | O_APPEND);
-	fw = write(fo, text_content, strlen(text_content));
+	fw = write(fo, text_content, len);
 	if (fo == -1 || fw == -1)
 		return (-1);
 
