edd18187ae8dd87b4869ae1d27c105cc2d38dc8e
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-13 01:22:07 +0300

    0x15-file_io

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to head of list
+ * @index: index of node to return
+ * Return: nth node of list, or NULL if node doesn't exist
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = NULL;
+	dlistint_t *node = *h;
+
+	if (h == NULL)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	node = get_dnodeint_at_index(*h, idx - 1);
+	if (node == NULL)
+		return (NULL);
+	new_node->next = node->next;
+	new_node->prev = node;
+	node->next = new_node;
+	if (new_node->next != NULL)
+		(new_node->next)->prev = new_node;
+	return (new_node);
+}
