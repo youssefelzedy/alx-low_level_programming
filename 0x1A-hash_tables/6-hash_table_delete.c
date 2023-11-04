e27c5068dca09b39aa05e71e01d7baf41b2d5733
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-04 02:43:45 +0200

    adding task seven

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,23 +9,23 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *temp, *temp2;
+	unsigned long int i;
+	hash_node_t *temp, *temp2;
 
-    if (ht == NULL)
-        return;
-    for (i = 0; i < ht->size; i++)
-    {
-        temp = ht->array[i];
-        while (temp != NULL)
-        {
-            temp2 = temp->next;
-            free(temp->key);
-            free(temp->value);
-            free(temp);
-            temp = temp2;
-        }
-    }
-    free(ht->array);
-    free(ht);
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			temp2 = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = temp2;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
