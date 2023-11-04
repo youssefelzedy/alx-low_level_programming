8010b29f82f9d1f5ed455b52bf17f656c87df379
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-04 02:40:29 +0200

    adding task six

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,24 +9,24 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *temp;
-    int flag = 0;
+	unsigned long int i;
+	hash_node_t *temp;
+	int flag = 0;
 
-    if (ht == NULL)
-        return;
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        temp = ht->array[i];
-        while (temp != NULL)
-        {
-            if (flag == 1)
-                printf(", ");
-            printf("'%s': '%s'", temp->key, temp->value);
-            flag = 1;
-            temp = temp->next;
-        }
-    }
-    printf("}\n");
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			flag = 1;
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
 }
