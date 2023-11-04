9dbb5d4d63d254b243b17c018e3553415655989f
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-04 02:43:34 +0200

    adding task seven

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
+ *
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_node_t *temp, *temp2;
+
+    if (ht == NULL)
+        return;
+    for (i = 0; i < ht->size; i++)
+    {
+        temp = ht->array[i];
+        while (temp != NULL)
+        {
+            temp2 = temp->next;
+            free(temp->key);
+            free(temp->value);
+            free(temp);
+            temp = temp2;
+        }
+    }
+    free(ht->array);
+    free(ht);
+}
