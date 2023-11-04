89b941a0db8da62cc739535d36c145e0a9b0314e
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-04 02:23:03 +0200

    adding task four

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,41 +11,41 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node = NULL, *temp = NULL;
+	unsigned long int index;
+	hash_node_t *new_node = NULL, *temp = NULL;
 
-    if (ht == NULL || key == NULL || value == NULL)
-        return (0);
-    index = key_index((const unsigned char *)key, ht->size);
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return (0);
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return (0);
-    }
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node);
-        return (0);
-    }
-    new_node->next = NULL;
-    temp = ht->array[index];
-    if (temp != NULL)
-    {
-        new_node->next = temp;
-        ht->array[index] = new_node;
-        return (1);
-    }
-    else
-    {
-        ht->array[index] = new_node;
-        return (1);
-    }
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = NULL;
+	temp = ht->array[index];
+	if (temp != NULL)
+	{
+		new_node->next = temp;
+		ht->array[index] = new_node;
+		return (1);
+	}
+	else
+	{
+		ht->array[index] = new_node;
+		return (1);
+	}
 
-    return (0);
+	return (0);
 }
